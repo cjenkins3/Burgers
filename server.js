f3adb4253ed8283f0b7ab7eb518bdc65b93491f3
Jenkins <cajenkins@smu.edu>
2019-01-25 12:47:10 -0600

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
 //Define port the server will be listening on.
 var PORT = process.env.PORT || 3000;
@@ -12,6 +13,9 @@ app.use(express.static(__dirname + '/public'));
 
 //Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
 //Parse application/json
 app.use(bodyParser.json());
